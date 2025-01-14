3e2f3443731f0f119555e577290435a763f94926
Nikita <manunknown964@gmail.com>
2025-01-14 17:54:34 +0500

    Small change: disabled eslint rule no-shadow in file where context of function allows using name of variable again

--- backend/src/trpc.ts
+++ backend/src/trpc.ts
@@ -20,6 +20,7 @@ export const trpcRouter = trpc.router({
       })
     )
     .query(({ input }) => {
+      // eslint-disable-next-line no-shadow
       const idea = ideas.find((idea) => idea.nick === input.ideaNick)
       return { idea: idea || null }
     }),
