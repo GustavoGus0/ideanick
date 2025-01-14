f490c1b34654c0e19037aac755cfa6e1b2694107
Nikita <manunknown964@gmail.com>
2025-01-14 16:39:53 +0500

    Enhanced typifying of react router dom and params from url. Now, creating new paths and links comes without typing new strings, only using special functions, where we define one string for all files

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -2,7 +2,7 @@ import AllIdeasPage from './pages/AllIdeasPage/AllIdeasPage'
 import ViewIdeaPage from './pages/ViewIdeaPage/ViewIdeaPage'
 import { TrpcProvider } from './lib/trpc'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { getAllIdeasRoute, getViewIdeaRoute } from './lib/routes'
+import { getAllIdeasRoute, getViewIdeaRoute, viewIdeaRouteParams } from './lib/routes'
 
 export const App = () => {
   return (
@@ -10,7 +10,7 @@ export const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path={getAllIdeasRoute()} element={<AllIdeasPage />} />
-          <Route path={getViewIdeaRoute({ ideaNick: ':ideaNick' })} element={<ViewIdeaPage />} />
+          <Route path={getViewIdeaRoute(viewIdeaRouteParams)} element={<ViewIdeaPage />} />
         </Routes>
       </BrowserRouter>
     </TrpcProvider>
