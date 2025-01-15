34cb7afe2b1db712dcb2d7fb784aea5fe88c9380
Nikita <manunknown964@gmail.com>
2025-01-15 21:43:43 +0500

    Created reusable component called Segment to watch DRY rule. Segment contains title, title's size, description and inner content of type ReactNode

--- webapp/src/pages/AllIdeasPage/AllIdeasPage.tsx
+++ webapp/src/pages/AllIdeasPage/AllIdeasPage.tsx
@@ -1,6 +1,7 @@
 import css from './index.module.scss'
 import { getViewIdeaRoute } from '../../lib/routes'
 import { Link } from 'react-router-dom'
+import Segment from '../../components/Segment/Segment'
 import { trpc } from '../../lib/trpc'
 
 export default function AllIdeasPage() {
@@ -15,20 +16,22 @@ export default function AllIdeasPage() {
   }
 
   return (
-    <div>
-      <h1 className={css.title}>Ideanick</h1>
+    <Segment title={'All Ideas'}>
       <div className={css.ideas}>
         {data.ideas.map((idea) => (
           <div className={css.idea} key={idea.nick}>
-            <h2 className={css.ideaName}>
-              <Link className={css.ideaLink} to={getViewIdeaRoute({ ideaNick: idea.nick })}>
-                {idea.name}
-              </Link>
-            </h2>
-            <p className={css.ideaDescription}>{idea.description}</p>
+            <Segment
+              size={2}
+              title={
+                <Link className={css.ideaLink} to={getViewIdeaRoute({ ideaNick: idea.nick })}>
+                  {idea.name}
+                </Link>
+              }
+              description={idea.description}
+            />
           </div>
         ))}
       </div>
-    </div>
+    </Segment>
   )
 }
--- webapp/src/pages/ViewIdeaPage/ViewIdeaPage.tsx
+++ webapp/src/pages/ViewIdeaPage/ViewIdeaPage.tsx
@@ -1,4 +1,5 @@
 import css from './index.module.scss'
+import Segment from '../../components/Segment/Segment'
 import { trpc } from '../../lib/trpc'
 import { useParams } from 'react-router-dom'
 import { ViewIdeaRouteParams } from '../../lib/routes'
@@ -23,10 +24,8 @@ export default function ViewIdeaPage() {
   }
 
   return (
-    <div>
-      <h1 className={css.title}>{data.idea.name}</h1>
-      <p className={css.description}>{data.idea.description}</p>
+    <Segment title={data.idea.name} description={data.idea.description}>
       <div className={css.text} dangerouslySetInnerHTML={{ __html: data.idea.text }} />
-    </div>
+    </Segment>
   )
 }
