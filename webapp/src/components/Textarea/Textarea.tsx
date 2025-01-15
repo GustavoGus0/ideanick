6bfdcf32398437e9a9a06d6e3d63d55d0bc83659
Nikita <manunknown964@gmail.com>
2025-01-16 01:27:22 +0500

    Created reusable components Input and Textarea to use in NewIdeaPage

---
+++ webapp/src/components/Textarea/Textarea.tsx
@@ -0,0 +1,28 @@
+import { Dispatch, SetStateAction } from 'react'
+export default function Textarea({
+  name,
+  label,
+  state,
+  setState,
+}: {
+  name: string
+  label: string
+  state: Record<string, any>
+  // This any is allowed to be used in this place
+  setState: Dispatch<SetStateAction<any>>
+}) {
+  return (
+    <div style={{ marginBottom: 10 }}>
+      <label htmlFor={name}>{label}</label>
+      <br />
+      <textarea
+        onChange={(e) => {
+          setState({ ...state, [name]: e.target.value })
+        }}
+        value={state[name]}
+        name={name}
+        id={name}
+      />
+    </div>
+  )
+}
