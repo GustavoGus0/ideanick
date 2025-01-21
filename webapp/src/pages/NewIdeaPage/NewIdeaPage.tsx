0c8702864f4dba0299a9acd95317d57fec6c8573
GustavoGus0 <manunknown964@gmail.com>
2025-01-21 20:59:19 +0500

    Moved zod validation block of code as it repeats in backend and frontend

--- webapp/src/pages/NewIdeaPage/NewIdeaPage.tsx
+++ webapp/src/pages/NewIdeaPage/NewIdeaPage.tsx
@@ -3,8 +3,8 @@ import Segment from '../../components/Segment/Segment'
 import Textarea from '../../components/Textarea/Textarea'
 import { useFormik } from 'formik'
 import { withZodSchema } from 'formik-validator-zod'
-import z from 'zod'
 import { trpc } from '../../lib/trpc'
+import { zCreateIdeaTrpcInput } from '@ideanick/backend/src/router/createIdea/input'
 
 export default function NewIdeaPage() {
   const createIdea = trpc.createIdea.useMutation()
@@ -15,17 +15,7 @@ export default function NewIdeaPage() {
       description: '',
       text: '',
     },
-    validate: withZodSchema(
-      z.object({
-        name: z.string().min(1),
-        nick: z
-          .string()
-          .min(1)
-          .regex(/^[a-z0-9-]+$/, 'Nick may contain only lowercase letters, numbers and hyphens'),
-        description: z.string().min(1),
-        text: z.string().min(100, 'Text should be at least 100 characters long'),
-      })
-    ),
+    validate: withZodSchema(zCreateIdeaTrpcInput),
     onSubmit: async (values) => {
       await createIdea.mutateAsync(values)
     },
