33e515a60df10c493c91b1bc08e710e9be93e626
GustavoGus0 <manunknown964@gmail.com>
2025-01-28 17:11:44 +0500

    Created styles for input and textarea components. Installed lib classnames to make optional classes conveniently

--- webapp/src/components/Textarea/Textarea.tsx
+++ webapp/src/components/Textarea/Textarea.tsx
@@ -1,15 +1,24 @@
 import { FormikProps } from 'formik'
+import css from './index.module.scss'
+import cn from 'classnames'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default function Textarea({ name, label, formik }: { name: string; label: string; formik: FormikProps<any> }) {
   const value = formik.values[name]
   const error = formik.errors[name] as string | undefined
   const touched = formik.touched[name]
+  const invalid = !!touched && !!error
+  const disabled = formik.isSubmitting
   return (
-    <div style={{ marginBottom: 10 }}>
-      <label htmlFor={name}>{label}</label>
-      <br />
+    <div className={cn({ [css.field]: true, [css.disabled]: disabled })}>
+      <label className={css.label} htmlFor={name}>
+        {label}
+      </label>
       <textarea
+        className={cn({
+          [css.input]: true,
+          [css.invalid]: invalid,
+        })}
         onChange={(e) => {
           void formik.setFieldValue(name, e.target.value)
         }}
@@ -21,7 +30,7 @@ export default function Textarea({ name, label, formik }: { name: string; label:
         id={name}
         disabled={formik.isSubmitting}
       />
-      {!!touched && !!error && <div style={{ color: 'red' }}>{error}</div>}
+      {invalid && <div className={css.error}>{error}</div>}
     </div>
   )
 }
