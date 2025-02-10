7ac4c803b140b38edbf96594a0993ac2e170c817
GustavoGus0 <manunknown964@gmail.com>
2025-02-10 11:08:48 +0500

    Added author to ideas

--- backend/src/router/getIdea/index.ts
+++ backend/src/router/getIdea/index.ts
@@ -10,6 +10,14 @@ export const getIdeaTrpcRoute = trpc.procedure
   .query(async ({ ctx, input }) => {
     const idea = await ctx.prisma.idea.findUnique({
       where: { nick: input.ideaNick },
+      include: {
+        author: {
+          select: {
+            id: true,
+            nick: true,
+          },
+        },
+      },
     })
     return { idea }
   })
