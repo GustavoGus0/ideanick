08fc81ef6979ef3f0de8996b392d16568eb7d352
GustavoGus0 <manunknown964@gmail.com>
2025-02-15 18:35:32 +0500

    Added checkbox to show only my ideas. The value to show ideas is stored in browser's Cookie

--- webapp/src/pages/AllIdeasPage/index.tsx
+++ webapp/src/pages/AllIdeasPage/index.tsx
@@ -3,9 +3,25 @@ import { getViewIdeaRoute } from '../../lib/routes'
 import { Link } from 'react-router-dom'
 import Segment from '../../components/Segment'
 import { trpc } from '../../lib/trpc'
+import { useEffect, useState } from 'react'
+import { useMe } from '../../lib/ctx'
+import { type TrpcRouterOutput } from '@ideanick/backend/src/router'
+import Cookies from 'js-cookie'
 
 export default function AllIdeasPage() {
+  const me = useMe()
+  const [onlyMyIdeas, setOnlyMyIdeas] = useState<boolean>(Cookies.get('onlyMyIdeas') === 'true' ? true : false)
   const { data, error, isLoading, isFetching, isError } = trpc.getIdeas.useQuery()
+
+  type Idea = TrpcRouterOutput['getIdeas']['ideas'][number]
+
+  useEffect(() => {
+    if (me) {
+      Cookies.set('onlyMyIdeas', onlyMyIdeas.toString(), { expires: 7 })
+    } else {
+      Cookies.remove('onlyMyIdeas')
+    }
+  }, [me])
 
   if (isLoading || isFetching) {
     return <span>Loading...</span>
@@ -15,10 +31,26 @@ export default function AllIdeasPage() {
     return <span>Error: {error.message}</span>
   }
 
+  const ideasToReturn = ({ ideas }: { ideas: Idea[] }) => {
+    return onlyMyIdeas ? ideas.filter((idea) => idea.authorId === me?.id) : ideas
+  }
+
   return (
-    <Segment title={'All Ideas'}>
+    <Segment title={onlyMyIdeas ? 'My Ideas' : 'All Ideas'}>
       <div className={css.ideas}>
-        {data.ideas.map((idea) => (
+        <div style={me ? { display: 'flex', alignItems: 'center', gap: 5, marginBottom: 10 } : { display: 'none' }}>
+          <input
+            type="checkbox"
+            name="show-only-my-ideas"
+            checked={onlyMyIdeas}
+            onChange={(e) => {
+              Cookies.set('onlyMyIdeas', e.target.checked.toString(), { expires: 7 })
+              return setOnlyMyIdeas(e.target.checked)
+            }}
+          />
+          <label htmlFor="show-only-my-ideas">Only my ideas</label>
+        </div>
+        {ideasToReturn(data).map((idea) => (
           <div className={css.idea} key={idea.nick}>
             <Segment
               size={2}
